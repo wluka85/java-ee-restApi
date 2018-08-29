3ffac03074e9a28958e3e3725aa7d0185565003e
Polikkox <damian.targosz11@gmail.com>
2018-08-29 15:40:25 +0200

    Add JSON parser

---
+++ src/main/java/com/codecool/restapi/JSON/JSONParser.java
@@ -0,0 +1,33 @@
+package com.codecool.restapi.JSON;
+
+import com.codecool.restapi.model.Phone;
+import com.codecool.restapi.model.ServiceInfo;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.json.JsonObject;
+import java.util.List;
+
+public class JSONParser {
+
+    private JSONObject jsonObject = new JSONObject();
+
+    public JSONObject generateJSONByList(List<Phone> data){
+
+        for(Phone phone : data){
+            JSONArray jsonArray = new JSONArray();
+
+            jsonArray.add(phone.getBrand());
+            jsonArray.add(phone.getModel());
+            for(ServiceInfo serviceInfo : phone.getServiceHistory()){
+                jsonArray.add(serviceInfo.getDescription());
+                jsonArray.add(serviceInfo.getPrice());
+                jsonArray.add(serviceInfo.getAnnotation());
+
+            }
+            this.jsonObject.put(phone.getClient().getName(), jsonArray);
+        }
+        System.out.println(jsonObject);
+        return jsonObject;
+    }
+}
